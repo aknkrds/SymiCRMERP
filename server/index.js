4a7771408f8078d3d5c55c1caea721c7400f6388
Akın Karadaş <aknkrds@hotmail.com>
2026-01-27 01:00:57 +0300

    fix: Update Express catch-all route to avoid path-to-regexp error
    - Use /:slug* instead of * or /* which are deprecated/stricter in newer Express/path-to-regexp versions

--- server/index.js
+++ server/index.js
@@ -726,7 +726,12 @@ app.use(express.static(path.join(__dirname, '../dist')));
 
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
-app.get('/*', (req, res) => {
+app.get('/:slug*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../dist/index.html'));
+});
+
+// Root route
+app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../dist/index.html'));
 });
 
